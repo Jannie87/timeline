6890a975122f82e67bf6ffc3e515664332490128
Jannie87 <jannie.backman@gmail.com>
2021-11-23 15:58:28 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function showHiddenInfo(){
+    let info = document.getElementById("itemOne");
+    if (info.style.display === "none") {
+        info.style.display ="block";
+    } else {
+        info.style.display ="none";
+    }
+}
